f1d2ecdefb3b48f4ffc6c0ffb187aed548334b28
Siv <sa.galkin94@gmail.com>
2022-09-11 05:59:15 +0300

    fix: fixed User schema

--- internal/migrations/000001_users_table.up.sql
+++ internal/migrations/000001_users_table.up.sql
@@ -1,7 +1,9 @@
 create TABLE users
 (
     id         UUID        DEFAULT gen_random_uuid() not null unique,
-    email      TEXT                                  not null,
+    login      TEXT                                  not null,
     password   TEXT                                  not null,
     created_at TIMESTAMPTZ default now()
 );
+
+create index index_login_users on users (login);
