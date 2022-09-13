91f2b81af77e06d9e231a5a9ee5a31330340ce98
Siv <sa.galkin94@gmail.com>
2022-09-13 12:55:41 +0300

    fix: ignoring user password in struct

--- internal/server/model/user.go
+++ internal/server/model/user.go
@@ -5,5 +5,5 @@ import "github.com/google/uuid"
 type User struct {
 	ID       uuid.UUID `json:"id"`
 	Login    string    `json:"login"`
-	Password string    `json:"password"`
+	Password string    `json:"-"`
 }
